90515e81ca0e8330feafe7a7681d6cd8996298f2
Dvd <dvdnet22@gmail.com>
2024-02-09 19:06:25 -0600

    xd

--- main.js
+++ main.js
@@ -21,7 +21,7 @@ function gay() {
     title="YouTube video player" 
     frameborder="0" 
     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
-    allowfullscreen></iframe>`;
+    allowfullscreen> </iframe>`;
 }
 
 function reload() {
